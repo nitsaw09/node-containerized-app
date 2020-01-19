5eb1e6fff6eca7dcaa2bc7d648b37a7298e2304c
nitesh.ghuge <nitesh.ghuge@encapto.com>
2020-01-19 20:01:26 +0530

    mean app changes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const Mongo = require("mongoose");
 
 const app = express();
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT);
+Mongo.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log(`DB connected`);
+});
+
+// start listening the server at port 3000
+app.listen(3000);
