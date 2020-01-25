eb003f21b4e4cd3d823216ffeeac0a346ba4e9e2
nitesh.ghuge <nitesh.ghuge@encapto.com>
2020-01-25 20:32:43 +0530

    error handling

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const app = express();
 
 const productRoutes = require("./api/routes/products");
 
+app.use(morgan("dev"));
+
 app.use("/products", productRoutes);
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Rest API");
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  res.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status = error.status || 500;
+  res.json({
+    message: error.message
+  });
+  next();
 });
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
