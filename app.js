f9b2ea9203785e66addd59e9e33f75b4f6cd41df
nitesh.ghuge <nitesh.ghuge@encapto.com>
2020-07-26 21:16:51 +0530

    updated the comments in app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   next(error);
 });
 
-// handling 404 & 500 error
+// handling 404 & 500 error response
 app.use((error, req, res, next) => {
   const resStatus = error.status || 500;
   res.status(resStatus).json({
